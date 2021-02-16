f504820cbb83d29461c029d66b00fd4d5a6b340b
NDPenaloza <ndpenaloza@gmail.com>
2021-02-16 00:13:46 -0800

    worked on Header-index.js

---
+++ directory/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Employee Directory!!</h1>
+            <p>Search! Sort!</p>
+        </header>
+    );
+}
+
+export default Header;
