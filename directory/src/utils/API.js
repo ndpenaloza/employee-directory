24dfd51a9474d24a23d24621afabe1507f7f0aea
NDPenaloza <ndpenaloza@gmail.com>
2021-02-15 23:51:16 -0800

    worked on random user generator in api.js

--- directory/src/utils/API.js
+++ directory/src/utils/API.js
@@ -1,1 +1,9 @@
+// Dependencies
 import axios from 'axios';
+
+// Random User Generator 
+export default {
+    roster: function() {
+        return axios.get('https://randomuser.me/api/?results=100')
+    }
+};
