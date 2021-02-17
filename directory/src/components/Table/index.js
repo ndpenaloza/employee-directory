a58f7db8c40b1559de3b7e30725660e42135f2d0
NDPenaloza <ndpenaloza@gmail.com>
2021-02-17 01:09:56 -0800

    worked on table.js

--- directory/src/components/Table/index.js
+++ directory/src/components/Table/index.js
@@ -1,1 +1,47 @@
 import React from 'react';
+import API from '../../utils/API';
+import { Component } from "react";
+
+
+class Table extends Component {
+    state = {
+        results: []
+    }
+
+
+// After component loads, run searchRoster
+componentDidMount = () => {
+    this.searchRoster();
+}
+
+searchRoster = () => {
+    API.roster()
+    .then((res) => 
+        this.setState({ results: res.data.results})
+    );
+    };
+
+    render() {
+        return(
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>    
+                            <td ></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default Table;
