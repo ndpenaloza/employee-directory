d920cce5e30a52f1764fbdc4247252cb89625634
NDPenaloza <ndpenaloza@gmail.com>
2021-02-16 23:16:45 -0800

    worked on footer

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,12 +1,16 @@
 // Dependencies
 import './App.css';
+import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Table from './components/Table';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Navbar/>
+      <Table/>
       <Footer/>
     </div>
   );
--- directory/src/components/Footer/index.js
+++ directory/src/components/Footer/index.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+function Footer () {
+    return (
+        <div className="footer">
+            <span className="text-muted text-center">Employee Directory!! &copy; 2021</span>
+        </div>
+    )
+}
+
+export default Footer;
